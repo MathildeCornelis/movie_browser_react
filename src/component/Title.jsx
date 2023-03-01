975e003f1947a24600275cd33cf7583a35b53e05
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-03-01 10:37:45 +0100

    Title component

--- src/component/Title.jsx
+++ src/component/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = () => {
     return (
-        <section className='flex justify-start p-5 text-xl font-lato text-white bg-background'>
+        <section className='flex justify-start pl-5 pt-7 pb-7 text-xl font-lato text-white bg-background'>
             <h1>
                 <span className='font-bold text-transparent bg-clip-text bg-gradient-to-t from-orange_dark to-orange_light'>Movie</span>Browser
             </h1>
