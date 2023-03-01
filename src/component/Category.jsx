c68d118cf7946fec14edeff67cd8aa2a35742b09
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-03-01 16:18:06 +0100

    category component

---
+++ src/component/Category.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Category = () => {
+    return (
+        <section className='font-lato text-white w-ful ml-5 mt-5 overflow-x-scroll'>
+            <ul className='flex flex-row text-l'>
+                <p className=''>Fantasy Horror Science Fiction Documentary petitchat oignonFantasy Horror Science Fiction Documentary petitchat oignon</p>
+            </ul>
+        </section>
+    );
+};
+
+export default Category;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Title from '../component/Title';
 import Menu from '../component/Menu';
 import Research from '../component/Research';
+import Category from '../component/Category';
 
 const Search = () => {
     return (
         <>
             <Title />
             <Research />
+            <Category />
             <Menu />
         </>
     );
