f207b63124600fc0b5f8a966d4fa8ae4441ed08e
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-03-08 16:12:17 +0100

    responsive title

--- src/component/Title.jsx
+++ src/component/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = () => {
     return (
-        <section className='flex justify-start pl-5 pt-7 pb-7 text-2xl font-lato text-white bg-background'>
+        <section className='flex justify-start pl-5 pt-7 pb-7 text-2xl font-lato text-white bg-background xl:pl-10 xl:pt-14 xl:pb-14 xl:pr-10'>
             <h1 className='w-full flex flex-row'>
                 <p className='xl:w-1/2 xl:text-5xl xl:flex xl:items-center xl:ml-10'>
                     <span className='font-bold text-transparent bg-clip-text bg-gradient-to-t from-orange_dark to-orange_light'>
